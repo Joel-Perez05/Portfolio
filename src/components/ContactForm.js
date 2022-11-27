a273c20fe709760791231fdf425c0bc7dc2441f4
Joel Perez <joel_perez65@yahoo.com>
2022-11-27 00:17:53 -0600

    added contact form styling

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import { Col } from 'react-bootstrap';
 import emailjs from "@emailjs/browser"
 import TrackVisibility from 'react-on-screen';
 
@@ -26,16 +27,26 @@ const ContactForm = (props) => {
   };
 
   return (
-    <div>
-      <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" required />
-      <label>Email</label>
-      <input type="email" name="user_email" required />
-      <label>Message</label>
-      <textarea name="message" required/>
-      <input type="submit" value="Send" />
-    </form>
+    <div className='contact' id='connect'>
+      <h2>Get In Touch With Me</h2>
+      <TrackVisibility>
+        { ({isVisible}) => 
+        <form className={isVisible? "animate__animated animate__zoomIn" : "animate__animated animate__zoomOut"} ref={form} onSubmit={sendEmail}>
+          <Col size={12} className="px-1">
+            <input type="text" name='user_name' placeholder="Name" required/>
+          </Col>
+          <Col size={12} className="px-1">
+            <input type="email" name='user_email' placeholder="Email Address" required/>
+          </Col>
+          <Col size={12} className="px-1">
+            <textarea rows="6" placeholder="Message" name='message' required></textarea>
+          </Col>
+          <Col>
+            <button type="submit"><span>Send</span></button>
+          </Col>
+        </form>
+        }
+      </TrackVisibility>
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from '../img/logo.svg'
-import fblogo from '../img/fblogo.svg'
-import githublogo from '../img/githublogo.svg'
-import linkedinlogo from '../img/linkedinlogo.svg'
+import { HashLink } from "react-router-hash-link";
+import { BrowserRouter } from "react-router-dom";
+// import logo from '../img/logo.svg'
+import fblogo from '../img/fblogo.svg';
+import githublogo from '../img/githublogo.svg';
+import linkedinlogo from '../img/linkedinlogo.svg';
 
 const NavBar = (props) => {
   const [activeLink, setActiveLink] = useState("");
@@ -29,33 +31,37 @@ const NavBar = (props) => {
   }
 
   return (
-    <div>
-      <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
-        <Container>
-          {/* <Navbar.Brand href="#home">
-            <img src={logo} alt="Logo" />
-          </Navbar.Brand> */}
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home" className={activeLink === "home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("home")}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === "skills" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("skills")}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === "projects" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("projects")}>Projects</Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-              <div className='social-icon'>
-                <a href='https://www.linkedin.com/in/joel-perez1995/' target=' _blank'><img src={linkedinlogo} alt="linkedin logo" /></a>
-                <a href='https://www.facebook.com/joel.x.perez' target=' _blank'><img src={fblogo} alt="facebook logo" /></a>
-                <a href='https://github.com/Joel-Perez05' target=' _blank'><img src={githublogo} alt="github logo" /></a>
-              </div>
-              {/* <button className='vvd' onClick={() => console.log("connect")}><span>Let's Connect</span></button> */}
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
+          <Container>
+            {/* <Navbar.Brand href="#home">
+              <img src={logo} alt="Logo" />
+            </Navbar.Brand> */}
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+              <span className="navbar-toggler-icon"></span>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home" className={activeLink === "home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("home")}>Home</Nav.Link>
+                <Nav.Link href="#skills" className={activeLink === "skills" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("skills")}>Skills</Nav.Link>
+                <Nav.Link href="#projects" className={activeLink === "projects" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("projects")}>Projects</Nav.Link>
+              </Nav>
+              <span className="navbar-text">
+                <div className='social-icon'>
+                  <a href='https://www.linkedin.com/in/joel-perez1995/' target=' _blank'><img src={linkedinlogo} alt="linkedin logo" /></a>
+                  <a href='https://www.facebook.com/joel.x.perez' target=' _blank'><img src={fblogo} alt="facebook logo" /></a>
+                  <a href='https://github.com/Joel-Perez05' target=' _blank'><img src={githublogo} alt="github logo" /></a>
+                </div>
+                <HashLink to="#connect">
+                  <button className='vvd' onClick={() => console.log("connect")}><span>Let's Connect</span></button>
+                </HashLink>
+              </span>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </BrowserRouter>
   );
 };
 
